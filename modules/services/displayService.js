5997c82802164670609e49edf9a843e614cdb52e
prodaromero <pablo.roda.romero@gmail.com>
2020-01-12 21:08:58 +0100

    Mejorando funcionalidad: Sabine añadido, Eiryng en progreso

--- modules/services/displayService.js
+++ modules/services/displayService.js
@@ -14,3 +14,20 @@ function displayService(evt, cityName) {
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+function displayService2(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent2");
+
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
