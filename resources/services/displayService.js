67dd1b2ffa07a3906d4b6cb965bb51bd6dc98a06
prodaromero <pablo.roda.romero@gmail.com>
2020-08-08 21:49:52 +0200

    [+] Añadido buscador de materiales

--- resources/services/displayService.js
+++ resources/services/displayService.js
@@ -9,3 +9,22 @@ function displayService(evt, app, mode) {
     document.getElementById(mode).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+function getMaterial() {
+  var input, filter, table, tr, td, material, i, txtValue;
+  input = document.getElementById("inputValue");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("materialsTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    material = tr[i].getElementsByTagName("td")[0];
+    if (material) {
+      txtValue = material.textContent || material.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
