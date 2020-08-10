ee8c39285e07a8c141a1269cb20c5a26b9b70bf8
prodaromero <pablo.roda.romero@gmail.com>
2020-08-10 20:58:48 +0200

    [~] Se añade búsqueda en toda la tabla

--- resources/services/displayService.js
+++ resources/services/displayService.js
@@ -10,17 +10,42 @@ function displayService(evt, app, mode) {
     evt.currentTarget.className += " active";
 }
 
-function getMaterial() {
-  var input, filter, table, tr, td, material, i, txtValue;
+function myFunction() {
+  var input, filter, table, tr, td, i;
   input = document.getElementById("inputValue");
   filter = input.value.toUpperCase();
   table = document.getElementById("materialsTable");
   tr = table.getElementsByTagName("tr");
   for (i = 0; i < tr.length; i++) {
-    material = tr[i].getElementsByTagName("td")[0];
-    if (material) {
-      txtValue = material.textContent || material.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+    name = tr[i].getElementsByTagName("td")[0];
+    nrc = tr[i].getElementsByTagName("td")[9];
+    find = false;
+      if (name.innerHTML.toUpperCase().indexOf(filter) > -1 || nrc.innerHTML.indexOf(filter) > -1) {
+        find = true;
+      }
+    if (find) {
+      tr[i].style.display = "";
+    } else {
+      tr[i].style.display = "none";
+    }
+  }
+}
+
+function getMaterial() {
+  var input, filter, table, tr, i, j, columns, value, find;
+  input = document.getElementById("inputValue");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("materialsTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    columns = tr[i].getElementsByTagName("td");
+    find = false;
+    for (var j = 0; j < columns.length; j++) {
+      value = columns[j]
+      if (value.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        find = true;
+      }
+      if (find) {
         tr[i].style.display = "";
       } else {
         tr[i].style.display = "none";
